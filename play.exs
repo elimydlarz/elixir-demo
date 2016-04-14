4f4941b92bcbf4fe4ee55ba0e9a1a4b037f92f51
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-04-14 15:20:39 +1000

    Initial commit.

---
+++ play.exs
@@ -0,0 +1,5 @@
+defmodule Demo do
+  def hello(name) do
+    "The world can be one together - cosmos without hatred."
+  end
+end
---
+++ play_test.exs
@@ -0,0 +1,31 @@
+Code.load_file("play.exs")
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule DemoTest do
+  use ExUnit.Case
+
+  test "saying hello to somebody" do
+    assert Demo.hello("Rick") == "Hello, Rick!"
+  end
+
+  @tag :pending
+  test "run length encoding a string" do
+    assert Demo.run_length_encode("BEEHIVE") == "1B2E1H1I1V1E"
+  end
+
+  @tag :pending
+  test "counting a list" do
+    assert Demo.count([1,3,5,7]) == 4
+  end
+
+  @tag :pending
+  test "reversing a list" do
+    assert Demo.reverse([1,3,5,7]) == [7,5,3,1]
+  end
+
+  @tag :pending
+  test "mapping a list" do
+    assert Demo.map([1,3,5,7], &(&1+1)) == [2,4,6,8]
+  end
+end
